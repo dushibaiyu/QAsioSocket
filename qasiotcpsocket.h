c96c2af211275cc31da214b4ef66df432337aa0a
渡世白玉 <dushibaiyu@yahoo.com>
2014-12-08 18:50:38 +0800

    继承改为组合。

--- qasiotcpsocket.h
+++ qasiotcpsocket.h
@@ -5,28 +5,29 @@
 #include <QBuffer>
 #include "asio.hpp"
 
-class QAsioTcpSocket : public QIODevice,public asio::ip::tcp::socket
+class QAsioTcpSocket : public QIODevice//,public asio::ip::tcp::socket
 {
     Q_OBJECT
 public:
-    explicit QAsioTcpSocket(asio::io_service& io_service,const protocol_type& protocol, QObject *parent = 0);
-    QAsioTcpSocket(asio::io_service& io_service, QObject *parent = 0);
-    QAsioTcpSocket(asio::io_service& io_service, const endpoint_type& endpoint, QObject *parent = 0);
-    QAsioTcpSocket(asio::io_service& io_service,
-                   const protocol_type& protocol, const native_handle_type& native_socket, QObject *parent = 0);
+    explicit QAsioTcpSocket(asio::ip::tcp::socket * socket , QObject *parent = 0);
 
 signals:
 
-public slots:
-
+public:
+    qint64 write_sync(const QByteArray & data);
+    qint64 write_sync(const char * data, qint64 maxSize);
+    qint64 write_sync(const char * data);
 protected:
     void readHandler(const asio::error_code& error, std::size_t bytes_transferred);
     void writeHandler(const asio::error_code& error,std::size_t bytes_transferred);
+    void readData(char *data, qint64 maxlen);
+    void readLine(char *data, qint64 maxlen);
+    qint64 writeData(const char * data, qint64 maxSize);
 private:
     QBuffer buffer;
     char data_[2048] = {0};
     qint16 readPos = 0;
-//    asio::ip::tcp::socket socket_;
+    asio::ip::tcp::socket * socket_;
     Q_DISABLE_COPY(QAsioTcpSocket)
 };
 
