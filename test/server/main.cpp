617e1beb418b297b11ccd8b13a12da353ebc31c8
渡世白玉 <dushibaiyu@yahoo.com>
2015-02-04 16:22:48 +0800

    server端的客户端测试代码删除

--- test/server/main.cpp
+++ test/server/main.cpp
@@ -6,17 +6,7 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
-//    QAsioTcpSocket sock;
-//    sock.connectToHost("127.0.0.1",2048);
-//    QObject::connect(&sock,&QAsioTcpSocket::connected,[&](){
-//        qDebug() << "socketconned";
-//        sock.write(QByteArray("hahaha"));});
-//    QObject::connect(&sock,&QAsioTcpSocket::disconnected,[](){qDebug() << "disconnected";});
-//    QObject::connect(&sock,&QAsioTcpSocket::readReadly,[&](){qDebug() << "readReadly:" << sock.readAll();sock.disconnectFromHost();});
-//    QObject::connect(&sock,&QAsioTcpSocket::sentError,[&](const QString & site,const asio::error_code & erro_code){
-//        qDebug() << "erro:" << site << ":" << erro_code.value();
-//    });
+    
     TestServer server;
     server.listen();
 
